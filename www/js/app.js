6c408df94b9481eee81540ba2b4c101b02bb2609
adamz <58021562+ad4mny@users.noreply.github.com>
2021-12-29 01:58:13 +0800

    added logout function

--- www/js/app.js
+++ www/js/app.js
@@ -1,2 +1,9 @@
 var token = JSON.parse(localStorage.getItem('token'));
 var url = 'http://localhost/source-code-analysis-tool/';
+
+var logout = function () {
+
+    localStorage.clear();
+    location.replace('index.html');
+
+};
