1e635f6821da4525f64fae36496bf13c9e4600f4
adamz <58021562+ad4mny@users.noreply.github.com>
2021-12-29 16:23:39 +0800

    added live api url

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,6 @@
 var token = JSON.parse(localStorage.getItem('token'));
-var url = 'http://localhost/source-code-analysis-tool/';
+// var url = 'http://localhost/source-code-analysis-tool/';
+var url = 'https://source-code-analysis-tools.000webhostapp.com/';
 
 var logout = function () {
 
